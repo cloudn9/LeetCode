4f34b3094f65e795209e332c4432d3d6405532bf
Tanya Georgieva <tanya.georgieva@tcpos.com>
2022-07-23 17:05:28 +0200

    Add project files.

---
+++ Challenges/Easy/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Challenges.Easy
+{
+    public static class TwoSum
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            if (nums.Length < 2) return null;
+
+            var data = nums.Select((p, i) => new { Value = p, Index = i }).ToList();
+
+            for (int i = 0; i < data.Count() - 1; i++)
+            {
+                var first = data[i].Value;
+                var number = data.Skip(i + 1).FirstOrDefault(p => p.Value + first == target);
+
+                if (number != null && first + number.Value == target)
+                {
+                    return new int[] { i, number.Index };
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ChallengesTest/TwoSumTest.cs
@@ -0,0 +1,43 @@
+﻿using Challenges.Easy;
+
+using NUnit.Framework;
+
+
+namespace Challenges.Test
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [Test]
+        public static void Test1()
+        {
+            Assert.AreEqual(new int[] { 0, 1 },
+                TwoSum.Solution(new int[] { 2, 7, 11, 15 }, 9), "Error");
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            Assert.AreEqual(new int[] { 1, 2 },
+                TwoSum.Solution(new int[] { 3, 2, 4 }, 6), "Error");
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual(new int[] { 0, 1 },
+                TwoSum.Solution(new int[] { 3, 3 }, 6), "Error");
+        }
+
+
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual(new int[] { 0, 3 },
+                TwoSum.Solution(new int[] { 0, 4, 3, 0 }, 0), "Error");
+        }
+
+      
+
+    }
+}
